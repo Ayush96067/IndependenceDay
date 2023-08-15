ce357c7e2764f16ebaf04d6ec15358709e2dfbd1
Ayush96067 <123870463+Ayush96067@users.noreply.github.com>
2023-08-15 20:06:08 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -98,7 +98,7 @@ export default function App() {
   );
 
   useEffect(function () {
-    fetch("http://localhost:8000/questions")
+    fetch("https://github.com/Ayush96067/IndependenceDay/blob/master/questions.json")
       .then((res) => res.json())
       .then((data) => dispatch({ type: "dataRecieved", payload: data }))
       .catch((err) => dispatch({ type: "dataFailed" }));
